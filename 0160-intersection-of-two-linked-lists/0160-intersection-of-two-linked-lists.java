816e08c7351317c1cf1f4e3f2d596e0139a9ad3e
Vamshi-Anantha <124914610+Vamshi-Anantha@users.noreply.github.com>
2025-04-24 18:50:35 +0530

    Time: 277 ms (7.95%), Space: 48.7 MB (45.22%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int getSize(ListNode head){
+        int size = 0;
+        ListNode temp = head;
+        while(temp != null){
+            size++;
+            temp = temp.next;
+        }
+        return size;
+    }
+
+    public ListNode getNthNode(ListNode head, int index){
+        ListNode temp = head;
+        while(index != 0){
+            temp = temp.next;
+            index--;
+        }
+        return temp;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // int size1 = getSize(headA);
+        // int size2 = getSize(headB);
+
+        // int diff = Math.abs(size1 - size2);
+
+        // if(size1 > size2){
+        //     headA = getNthNode(headA, diff);
+        // }else{
+        //     headB = getNthNode(headB, diff);
+        // }
+
+        // while(headA != null && headB != null && headA != headB){
+        //     headA = headA.next;
+        //     headB = headB.next;
+        // }
+        // return headA;
+
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        while(temp1 != temp2){
+            temp1 = temp1 == null ? headA : temp1.next;
+            temp2 = temp2 == null ? headB : temp2.next;
+        }
+        return temp1;
+    }
+}
