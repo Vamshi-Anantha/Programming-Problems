85770081b62a1b3f3fde1622920f91044c17910d
Vamshi-Anantha <124914610+Vamshi-Anantha@users.noreply.github.com>
2025-04-24 19:27:28 +0530

    Time: 0 ms (100%), Space: 41.5 MB (64.4%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[] freq = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            int index = c - 'a';
+            if(freq[index]!= 0){
+                return s.charAt(i);
+            }else{
+                freq[index]++;
+            }
+           
+
+        }
+        return ' ';
+    }
+}
